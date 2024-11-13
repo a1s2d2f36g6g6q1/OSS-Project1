427630f8dcc3271107792b2e7eda41959a562b31
a1s2d2f36g6g6q1 <vrc.maya@chungbuk.ac.kr>
2024-11-13 14:32:46 +0900

    no message

--- main.c
+++ main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
   printf("Open Source SW Project\n");
   printf("GitHub Training");
+
+  printf("What happens if I add on main branch?");
   return 0;
 }
