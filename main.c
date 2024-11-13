eb98b83339914b5e897e70e98ea843711a532b7c
a1s2d2f36g6g6q1 <vrc.maya@chungbuk.ac.kr>
2024-11-13 14:34:17 +0900

    no message

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
 
-int main() {
+int main()
+{
+  int i = 0;
   printf("Open Source SW Project\n");
   printf("GitHub Training");
+
+  for (i = 0; i < 10; i++)
+  {
+    printf("Test Branch\n");
+  }
   return 0;
 }
